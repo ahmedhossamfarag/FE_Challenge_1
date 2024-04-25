8aacb714431a5156db380a58531b11c05f0801e0
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2024-04-25 09:50:02 +0200

    Add project files.

---
+++ users.js
@@ -0,0 +1,705 @@
+const users_json = 
+`[
+	{
+		"username": "Chastity Bishop",
+		"country": "Belgium",
+		"since": "18.08.23",
+		"sales": 8305,
+		"articles": 2381076
+	},
+	{
+		"username": "Evan Barrera",
+		"country": "New Zealand",
+		"since": "17.06.24",
+		"sales": 94015,
+		"articles": 6119265
+	},
+	{
+		"username": "Meredith Barker",
+		"country": "Brazil",
+		"since": "24.05.23",
+		"sales": 79282,
+		"articles": 8685712
+	},
+	{
+		"username": "Brendan Ferguson",
+		"country": "Turkey",
+		"since": "10.09.23",
+		"sales": 69970,
+		"articles": 4874884
+	},
+	{
+		"username": "Daniel Hobbs",
+		"country": "Singapore",
+		"since": "09.02.24",
+		"sales": 71161,
+		"articles": 9058068
+	},
+	{
+		"username": "Zahir Henry",
+		"country": "New Zealand",
+		"since": "30.06.24",
+		"sales": 11854,
+		"articles": 738804
+	},
+	{
+		"username": "Wendy Kramer",
+		"country": "Nigeria",
+		"since": "05.07.23",
+		"sales": 61436,
+		"articles": 734183
+	},
+	{
+		"username": "Shellie Leblanc",
+		"country": "Brazil",
+		"since": "20.10.23",
+		"sales": 70015,
+		"articles": 9933428
+	},
+	{
+		"username": "Thaddeus Martin",
+		"country": "Spain",
+		"since": "31.10.23",
+		"sales": 2590,
+		"articles": 2788397
+	},
+	{
+		"username": "Hannah Montoya",
+		"country": "Vietnam",
+		"since": "29.05.24",
+		"sales": 73360,
+		"articles": 8413470
+	},
+	{
+		"username": "Velma Rodriquez",
+		"country": "Poland",
+		"since": "01.10.24",
+		"sales": 87382,
+		"articles": 9358077
+	},
+	{
+		"username": "Thane Crane",
+		"country": "United Kingdom",
+		"since": "07.02.25",
+		"sales": 32749,
+		"articles": 4848034
+	},
+	{
+		"username": "Donna Castillo",
+		"country": "Chile",
+		"since": "07.07.23",
+		"sales": 59931,
+		"articles": 6745742
+	},
+	{
+		"username": "Belle Whitfield",
+		"country": "Norway",
+		"since": "10.07.24",
+		"sales": 3659,
+		"articles": 2126196
+	},
+	{
+		"username": "Seth Sherman",
+		"country": "Indonesia",
+		"since": "11.10.24",
+		"sales": 99924,
+		"articles": 9142975
+	},
+	{
+		"username": "Lars Blackburn",
+		"country": "Canada",
+		"since": "08.11.23",
+		"sales": 80111,
+		"articles": 4569214
+	},
+	{
+		"username": "Haviva Tanner",
+		"country": "Netherlands",
+		"since": "09.02.24",
+		"sales": 7487,
+		"articles": 6763350
+	},
+	{
+		"username": "Salvador Holland",
+		"country": "Singapore",
+		"since": "25.04.23",
+		"sales": 78257,
+		"articles": 5678935
+	},
+	{
+		"username": "Aiko Mosley",
+		"country": "Ireland",
+		"since": "27.06.24",
+		"sales": 7451,
+		"articles": 292292
+	},
+	{
+		"username": "Jakeem Lott",
+		"country": "South Korea",
+		"since": "26.02.24",
+		"sales": 60328,
+		"articles": 4122978
+	},
+	{
+		"username": "Alika Lawson",
+		"country": "Netherlands",
+		"since": "11.10.23",
+		"sales": 19650,
+		"articles": 3894393
+	},
+	{
+		"username": "Dahlia Bowers",
+		"country": "Nigeria",
+		"since": "07.04.23",
+		"sales": 34863,
+		"articles": 7766609
+	},
+	{
+		"username": "Frances Rogers",
+		"country": "United Kingdom",
+		"since": "04.12.23",
+		"sales": 29378,
+		"articles": 9471917
+	},
+	{
+		"username": "Ali Wolfe",
+		"country": "China",
+		"since": "22.11.24",
+		"sales": 45565,
+		"articles": 7652425
+	},
+	{
+		"username": "Brooke Sargent",
+		"country": "United Kingdom",
+		"since": "20.05.24",
+		"sales": 43593,
+		"articles": 5550555
+	},
+	{
+		"username": "Kevyn Meyer",
+		"country": "Norway",
+		"since": "01.01.24",
+		"sales": 42623,
+		"articles": 1245345
+	},
+	{
+		"username": "Calista Richmond",
+		"country": "Nigeria",
+		"since": "01.07.23",
+		"sales": 84483,
+		"articles": 5307465
+	},
+	{
+		"username": "Lucas Murray",
+		"country": "Colombia",
+		"since": "11.09.23",
+		"sales": 84593,
+		"articles": 1111341
+	},
+	{
+		"username": "Breanna Fulton",
+		"country": "Chile",
+		"since": "09.11.24",
+		"sales": 31760,
+		"articles": 3029912
+	},
+	{
+		"username": "Shay Colon",
+		"country": "China",
+		"since": "08.12.23",
+		"sales": 93815,
+		"articles": 8193574
+	},
+	{
+		"username": "Gage Ellis",
+		"country": "Australia",
+		"since": "08.09.24",
+		"sales": 90331,
+		"articles": 1263163
+	},
+	{
+		"username": "MacKenzie Bryan",
+		"country": "Turkey",
+		"since": "17.08.24",
+		"sales": 22316,
+		"articles": 1054063
+	},
+	{
+		"username": "Ryan Dejesus",
+		"country": "Spain",
+		"since": "01.10.24",
+		"sales": 23397,
+		"articles": 7120346
+	},
+	{
+		"username": "Joan Owen",
+		"country": "South Africa",
+		"since": "26.10.24",
+		"sales": 51706,
+		"articles": 709868
+	},
+	{
+		"username": "Charles Mayer",
+		"country": "Singapore",
+		"since": "18.02.25",
+		"sales": 41494,
+		"articles": 696472
+	},
+	{
+		"username": "Patricia Mosley",
+		"country": "United Kingdom",
+		"since": "28.04.23",
+		"sales": 74554,
+		"articles": 8012391
+	},
+	{
+		"username": "Xenos Conner",
+		"country": "Norway",
+		"since": "21.03.23",
+		"sales": 49975,
+		"articles": 7234083
+	},
+	{
+		"username": "Yoshi Mckee",
+		"country": "Vietnam",
+		"since": "19.09.24",
+		"sales": 55525,
+		"articles": 1941713
+	},
+	{
+		"username": "Gavin Carver",
+		"country": "New Zealand",
+		"since": "12.04.23",
+		"sales": 4361,
+		"articles": 3468117
+	},
+	{
+		"username": "Maisie Robinson",
+		"country": "Norway",
+		"since": "26.08.23",
+		"sales": 14759,
+		"articles": 4666462
+	},
+	{
+		"username": "Willa Lawrence",
+		"country": "Turkey",
+		"since": "23.12.24",
+		"sales": 75903,
+		"articles": 5626733
+	},
+	{
+		"username": "Fleur Irwin",
+		"country": "Nigeria",
+		"since": "17.05.23",
+		"sales": 73160,
+		"articles": 8356572
+	},
+	{
+		"username": "Vincent Shields",
+		"country": "China",
+		"since": "10.04.23",
+		"sales": 99384,
+		"articles": 2228355
+	},
+	{
+		"username": "Preston Mcintyre",
+		"country": "Chile",
+		"since": "24.04.23",
+		"sales": 5444,
+		"articles": 5679633
+	},
+	{
+		"username": "Ulla Browning",
+		"country": "Russian Federation",
+		"since": "08.09.24",
+		"sales": 44827,
+		"articles": 4952974
+	},
+	{
+		"username": "Dawn Allen",
+		"country": "Ukraine",
+		"since": "27.06.24",
+		"sales": 60099,
+		"articles": 2877413
+	},
+	{
+		"username": "Jarrod Clay",
+		"country": "Colombia",
+		"since": "12.09.23",
+		"sales": 33593,
+		"articles": 506952
+	},
+	{
+		"username": "Nell Compton",
+		"country": "Austria",
+		"since": "02.07.23",
+		"sales": 70109,
+		"articles": 6150170
+	},
+	{
+		"username": "Hakeem Villarreal",
+		"country": "China",
+		"since": "18.10.23",
+		"sales": 6287,
+		"articles": 2555960
+	},
+	{
+		"username": "Brenda Anderson",
+		"country": "Singapore",
+		"since": "02.04.23",
+		"sales": 13211,
+		"articles": 3211992
+	},
+	{
+		"username": "Brenda Ochoa",
+		"country": "Germany",
+		"since": "31.12.24",
+		"sales": 83191,
+		"articles": 3961036
+	},
+	{
+		"username": "Nora Fitzgerald",
+		"country": "United States",
+		"since": "18.04.24",
+		"sales": 90332,
+		"articles": 1005681
+	},
+	{
+		"username": "Velma Solis",
+		"country": "Colombia",
+		"since": "06.11.24",
+		"sales": 42366,
+		"articles": 9514355
+	},
+	{
+		"username": "Amaya Hobbs",
+		"country": "Netherlands",
+		"since": "08.04.24",
+		"sales": 89295,
+		"articles": 1886201
+	},
+	{
+		"username": "Geraldine Camacho",
+		"country": "Nigeria",
+		"since": "07.06.23",
+		"sales": 14787,
+		"articles": 2599260
+	},
+	{
+		"username": "Elliott Cash",
+		"country": "Austria",
+		"since": "26.10.23",
+		"sales": 86158,
+		"articles": 1469633
+	},
+	{
+		"username": "Bianca Hull",
+		"country": "Norway",
+		"since": "08.11.23",
+		"sales": 30189,
+		"articles": 894569
+	},
+	{
+		"username": "Mason Herring",
+		"country": "China",
+		"since": "17.08.24",
+		"sales": 27374,
+		"articles": 9418815
+	},
+	{
+		"username": "Ethan Shaffer",
+		"country": "Poland",
+		"since": "14.12.23",
+		"sales": 11702,
+		"articles": 3457412
+	},
+	{
+		"username": "Zoe Dawson",
+		"country": "Germany",
+		"since": "22.10.24",
+		"sales": 63576,
+		"articles": 6662215
+	},
+	{
+		"username": "Madeline Branch",
+		"country": "Nigeria",
+		"since": "18.04.23",
+		"sales": 20995,
+		"articles": 4449662
+	},
+	{
+		"username": "Summer Holcomb",
+		"country": "Chile",
+		"since": "17.04.24",
+		"sales": 69984,
+		"articles": 3710892
+	},
+	{
+		"username": "Cruz Hardin",
+		"country": "China",
+		"since": "18.02.24",
+		"sales": 52088,
+		"articles": 3588033
+	},
+	{
+		"username": "Randall Wilder",
+		"country": "Vietnam",
+		"since": "12.08.24",
+		"sales": 20491,
+		"articles": 4686024
+	},
+	{
+		"username": "Kyle Kent",
+		"country": "Italy",
+		"since": "31.07.24",
+		"sales": 5098,
+		"articles": 6365840
+	},
+	{
+		"username": "Claudia Spencer",
+		"country": "Norway",
+		"since": "21.05.24",
+		"sales": 69578,
+		"articles": 7776291
+	},
+	{
+		"username": "Shelley Boyer",
+		"country": "New Zealand",
+		"since": "10.01.24",
+		"sales": 69048,
+		"articles": 6194696
+	},
+	{
+		"username": "Wynter Farmer",
+		"country": "Netherlands",
+		"since": "26.05.23",
+		"sales": 54639,
+		"articles": 5397200
+	},
+	{
+		"username": "Plato Bean",
+		"country": "Ukraine",
+		"since": "08.04.24",
+		"sales": 90285,
+		"articles": 6374522
+	},
+	{
+		"username": "Jane Jones",
+		"country": "Italy",
+		"since": "14.12.23",
+		"sales": 5766,
+		"articles": 5303980
+	},
+	{
+		"username": "Olivia O'donnell",
+		"country": "South Africa",
+		"since": "16.11.24",
+		"sales": 8064,
+		"articles": 7684702
+	},
+	{
+		"username": "Julian Rosario",
+		"country": "New Zealand",
+		"since": "22.03.23",
+		"sales": 70142,
+		"articles": 8416591
+	},
+	{
+		"username": "Allistair Mercer",
+		"country": "Austria",
+		"since": "17.01.25",
+		"sales": 78176,
+		"articles": 758859
+	},
+	{
+		"username": "Kamal Campbell",
+		"country": "United Kingdom",
+		"since": "28.06.23",
+		"sales": 40765,
+		"articles": 8527286
+	},
+	{
+		"username": "Amy Vega",
+		"country": "Brazil",
+		"since": "24.03.24",
+		"sales": 64311,
+		"articles": 7587145
+	},
+	{
+		"username": "Brent Wheeler",
+		"country": "Philippines",
+		"since": "11.06.23",
+		"sales": 56955,
+		"articles": 5519103
+	},
+	{
+		"username": "Whoopi Bradley",
+		"country": "Nigeria",
+		"since": "14.07.24",
+		"sales": 8658,
+		"articles": 9484033
+	},
+	{
+		"username": "Inez Hatfield",
+		"country": "New Zealand",
+		"since": "17.12.23",
+		"sales": 37933,
+		"articles": 7184544
+	},
+	{
+		"username": "Illiana English",
+		"country": "South Korea",
+		"since": "28.09.24",
+		"sales": 61714,
+		"articles": 5360633
+	},
+	{
+		"username": "Tatum Gill",
+		"country": "India",
+		"since": "12.02.25",
+		"sales": 93102,
+		"articles": 1899165
+	},
+	{
+		"username": "Beck Luna",
+		"country": "Chile",
+		"since": "19.01.25",
+		"sales": 62050,
+		"articles": 6318085
+	},
+	{
+		"username": "Lucy Carey",
+		"country": "Ireland",
+		"since": "26.01.24",
+		"sales": 65530,
+		"articles": 6175626
+	},
+	{
+		"username": "Kylan Frazier",
+		"country": "Pakistan",
+		"since": "28.09.23",
+		"sales": 76613,
+		"articles": 364679
+	},
+	{
+		"username": "Levi Frederick",
+		"country": "Philippines",
+		"since": "16.08.23",
+		"sales": 83550,
+		"articles": 7317780
+	},
+	{
+		"username": "Jolene English",
+		"country": "Ukraine",
+		"since": "01.05.24",
+		"sales": 95560,
+		"articles": 8848047
+	},
+	{
+		"username": "Wayne Alford",
+		"country": "Mexico",
+		"since": "22.07.24",
+		"sales": 41652,
+		"articles": 7132851
+	},
+	{
+		"username": "Macey Cannon",
+		"country": "Peru",
+		"since": "12.07.24",
+		"sales": 72149,
+		"articles": 3165199
+	},
+	{
+		"username": "Gannon Gonzalez",
+		"country": "China",
+		"since": "29.02.24",
+		"sales": 11455,
+		"articles": 3862916
+	},
+	{
+		"username": "Ryan Chen",
+		"country": "Ukraine",
+		"since": "04.10.24",
+		"sales": 67119,
+		"articles": 8088647
+	},
+	{
+		"username": "Fritz Fuller",
+		"country": "Costa Rica",
+		"since": "24.05.24",
+		"sales": 17426,
+		"articles": 691105
+	},
+	{
+		"username": "Violet Cardenas",
+		"country": "India",
+		"since": "26.06.24",
+		"sales": 48254,
+		"articles": 1751980
+	},
+	{
+		"username": "Chaney Merrill",
+		"country": "Nigeria",
+		"since": "28.12.23",
+		"sales": 74886,
+		"articles": 4552339
+	},
+	{
+		"username": "Kirk Chapman",
+		"country": "Nigeria",
+		"since": "31.08.23",
+		"sales": 44653,
+		"articles": 4094210
+	},
+	{
+		"username": "Odette Davenport",
+		"country": "Netherlands",
+		"since": "09.10.23",
+		"sales": 5727,
+		"articles": 3372162
+	},
+	{
+		"username": "Donna Rogers",
+		"country": "New Zealand",
+		"since": "16.04.23",
+		"sales": 17023,
+		"articles": 5271404
+	},
+	{
+		"username": "Rogan Malone",
+		"country": "Brazil",
+		"since": "07.12.24",
+		"sales": 20694,
+		"articles": 3078325
+	},
+	{
+		"username": "Justin Bradley",
+		"country": "Pakistan",
+		"since": "23.01.25",
+		"sales": 67410,
+		"articles": 2720997
+	},
+	{
+		"username": "Lacota Burks",
+		"country": "Russian Federation",
+		"since": "07.05.23",
+		"sales": 18134,
+		"articles": 5212564
+	},
+	{
+		"username": "Francesca Hyde",
+		"country": "Germany",
+		"since": "13.09.23",
+		"sales": 31458,
+		"articles": 1989732
+	},
+	{
+		"username": "Stuart Tyler",
+		"country": "Mexico",
+		"since": "25.03.24",
+		"sales": 82922,
+		"articles": 4290697
+	}
+]`
+
+const users = JSON.parse(users_json)
